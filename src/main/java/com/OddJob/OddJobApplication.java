842467c4e59af126f6a7e1d2a6d8e1b580fb8d00
Linus Eriksson <linusaxel@gmail.com>
2019-03-18 13:10:46 +0100

    intellij project

---
+++ src/main/java/com/OddJob/OddJobApplication.java
@@ -0,0 +1,13 @@
+package com.OddJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OddJobApplication.class, args);
+	}
+
+}
