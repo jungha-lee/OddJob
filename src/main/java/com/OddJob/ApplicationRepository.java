f0aa630da4ecd0ea5ca88530aa640c3c9324fdd5
Linus Eriksson <linusaxel@gmail.com>
2019-03-25 16:21:51 +0100

    rest controller

--- src/main/java/com/OddJob/ApplicationRepository.java
+++ src/main/java/com/OddJob/ApplicationRepository.java
@@ -1,9 +1,13 @@
 package com.OddJob;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ApplicationRepository extends CrudRepository<Application, Long> {
     List<Application> findJob_IdByApplicantId(User user);
+
+
 }
