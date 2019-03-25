3eb3660393480b217910b142e8f738bb3b5a6b59
Jungha Lee <junghalee001@gmail.com>
2019-03-25 16:42:46 +0100

    multiple place centering fix, but not search result yet

--- webapp/src/app/components/map-list/map-list.component.ts
+++ webapp/src/app/components/map-list/map-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Job } from 'src/app/models/job';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 
 @Component({
   selector: 'map-list',
@@ -11,17 +12,32 @@ export class MapListComponent implements OnInit {
   zoom: number = 10;
 
   // initial center position for the map
-  lat: number = 59.35;
-  lng: number = 18.0;
+  lat: number;
+  lng: number;
 
   // list of lat,lng for calcualting center of map
-  listLat: number[];
-  listLng: number[];
+  listLat: number[] = [];
+  listLng: number[] = [];
 
-  @Input() jobsForMap: Job[];
+  @Input() joblistForMap: Job[];
 
-  constructor() {}
+  calcMapCenter() {
+    console.log('5.calcMapCenter is doing something');
 
-  ngOnInit() {
+// tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.joblistForMap.length; i++) {
+      this.listLat.push(this.joblistForMap[i].location.lat);
+      this.listLng.push(this.joblistForMap[i].location.lng);
+    }
+    this.lat = (Math.max(...this.listLat) + Math.min(...this.listLat)) / 2;
+    this.lng = (Math.max(...this.listLng) + Math.min(...this.listLng)) / 2;
   }
+
+  constructor() {
+  }
+  
+  ngOnInit() {
+    this.calcMapCenter();
+  }
+
 }
--- webapp/src/app/components/search-page/search-page.component.ts
+++ webapp/src/app/components/search-page/search-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { JobService } from '../../services/job.service';
 import { Job } from '../../models/job';
 import { Directive } from '@angular/core';
@@ -15,32 +15,40 @@ export class SearchPageComponent implements OnInit {
   @Input() userInput: string;
   userSearch: string;
   selectedJob: Job;
+  jobDataLoaded: boolean;
+  searchedJobLoaded: boolean;
 
   constructor(injectedService: JobService) {
     this.service = injectedService;
   }
 
   ngOnInit() {
+    console.log('1. init start');
+    this.jobDataLoaded = false;
     this.service.getJobs().subscribe(jobs => {
       this.jobs = jobs;
-      console.log(jobs[1]);
       this.selectedJob = jobs[0];
-      console.log(this.selectedJob);
+      this.jobDataLoaded = true;
+      console.log('2. init end');
     });
-
   }
 
   searchMatches(job) {
+    console.log('3. searchMatches start');
+    this.searchedJobLoaded = false;
     const ifMatches: boolean =
       this.aContainsB(job.title, [this.userSearch]) ||
       this.aContainsB(job.description, [this.userSearch]) ||
       this.aContainsB(job.location.city, [this.userSearch]) ||
       this.aContainsB(job.location.country, [this.userSearch]);
+
     if (ifMatches) {
       if (this.searchedJobs.indexOf(job) === -1) {
         this.searchedJobs.push(job);
       }
     }
+    console.log('4. searchMatches end');
+    this.searchedJobLoaded = true;
     return ifMatches;
   }
 
